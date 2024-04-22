a950eff9832965f02dd591b12fe01c40bb1165b0
ARXARC <159053836+ARXARC@users.noreply.github.com>
2024-04-22 11:16:45 -0600

    Update lista-def.py

--- lista-def.py
+++ lista-def.py
@@ -1,18 +1,25 @@
 lista = []
-def agregar(item):
-  lista.append(item)
-def remover(item):
-  if item in lista:
-    lista.remove(item)
-def visualizar():
-  print(lista)
-while True:
+
+def menu():
   print("Opciones:")
   print("1. Añadir item")
   print("2. Remover item")
   print("3. Visualizar lista")
   print("4. Salir")
   opcion = int(input("Introduzca una opción: "))
+
+def agregar(item):
+  lista.append(item)
+
+def remover(item):
+  if item in lista:
+    lista.remove(item)
+    
+def visualizar():
+  print(lista)
+  
+while True:
+  menu()
   if opcion == 1:
     item = input("Introduzca el item a añadir: ")
     agregar(item)
